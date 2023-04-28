9b1246cb49430e9a9cf875a35eb37a5cc09ac8dc
Hitesh Tiwari <72449162+hit98gag@users.noreply.github.com>
2023-04-28 14:06:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Navs from './Comp/Navs';
+import Containers from './Comp/Container';
+import Contain2 from './Comp/Contain2';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navs/>
+      <Containers/>
+      <Contain2/>
     </div>
   );
 }
